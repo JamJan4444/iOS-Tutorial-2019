50fa1e3273f158f43b3034a1347891e4fd5a0ebe
Jan Rensing <jan.rensing@gmail.com>
2019-08-26 17:50:33 +0200

    Refactor Project Strucure

---
+++ Lesson02Project/Lesson02Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson02Project
+//
+//  Created by Jan Rensing on 26.08.19.
+//  Copyright © 2019 Jan Rensing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
